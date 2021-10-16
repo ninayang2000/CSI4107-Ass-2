c166dfc6654db2314a6975cee0c4830a5a0ae617
Nina <nina.yangny@hotmail.com>
2021-10-16 23:19:06 +1100

    fixed preprocessor

--- PreProcessor.java
+++ PreProcessor.java
@@ -1,8 +1,14 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.*;
+import java.util.regex.Matcher;
 
 public class PreProcessor {
 
@@ -13,37 +19,46 @@ public class PreProcessor {
 
         try {
 
+            // File input = new File("test.txt");
+
             File input = new File("Trec_microblog11.txt");
-            System.out.println("Opened successfully");
-            Scanner scanner = new Scanner(input);
 
-            while (scanner.hasNext()) {
-                // change word to lower case
-                String word = scanner.next().toLowerCase();
+            FileReader fr = new FileReader(input);
+            BufferedReader bufferReader = new BufferedReader(fr);
 
-                // remove special characters and numbers
-                word = word.replaceAll("[^a-zA-Z]","");
-                // store in array
-                if (word.length() > 0) {
-                    words.add(word);
+
+            String line;
+            while ((line = bufferReader.readLine()) != null) {
+
+                // remove urls and punctuation
+                line = line.replaceAll("http://[^\\s]+", "");
+                line = line.replaceAll("[^a-zA-Z]"," ");
+      
+
+                // split lines by space
+                String[] message = line.split("\\s+");
+                for (String word: message) {
+                    word = word.toLowerCase();
+                    if (word.length() > 0 && !word.contains("http")) {
+                        words.add(word);
+
+                    }
+
                 }
             }
 
-            // remove all the stop words
+              // remove all the stop words
             words.removeAll(stopWords);
-            // for (String word: words) {
-            //     System.out.println(word);
-            // }
 
-            PrintWriter writer = new PrintWriter("Processed.txt", "UTF-8");
-            for (String word: words) {
-                writer.println(word);
+            // write words into new file 
+            Writer writer = new FileWriter("PreProcessed.txt", false); //overwrites file
+            for (String word: words) { 
+                writer.write(word + System.lineSeparator());
+            
             }
 
             writer.close();
 
-            // close scanner
-            scanner.close();
         }   catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
