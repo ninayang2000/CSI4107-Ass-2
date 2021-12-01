30544bb2dfe47d428204dfdb2c87e72c9ec0b576
Nina <nina.yangny@hotmail.com>
2021-12-01 19:14:52 +1100

    working experiment 1

--- ver4.py
+++ ver4.py
@@ -3,7 +3,7 @@ import re
 import string 
 sen = []
 tweetIds = []
-with open("test.txt") as f:
+with open("Trec_microblog11.txt") as f:
     for line in f:
         tweetID = line[:18]
         tweet = line[18:]
@@ -20,21 +20,30 @@ from sentence_transformers import SentenceTransformer
 model = SentenceTransformer('bert-base-nli-mean-tokens')
 #Encoding:
 sen_embeddings = model.encode(sen)
-sen_embeddings.shape
 
-query = "Three years later, the coffin was still full of Jello"
-query_embed = model.encode(query)
+## queries 
+with open("topics_MB1-49.txt") as f:
+    for row in f:
+        if (row.startswith("<title>")):
+            start = row.find("<title>") + len("<title>")
+            end = row.find("</title>")
+            query = row[start:end]    
+            print(query)
 
-from sklearn.metrics.pairwise import cosine_similarity
-#let's calculate cosine similarity for sentence 0:
 
+            query_embed = model.encode(query)
 
-# print(cosine_similarity([query_embed],sen_embeddings[0:]))
+            from sklearn.metrics.pairwise import cosine_similarity
+            #let's calculate cosine similarity for sentence 0:
 
-for i in cosine_similarity([query_embed],sen_embeddings[0:]):
-    result = list(enumerate(i))
-    print(result)
-    list3 = sorted(result, reverse= True, key = lambda x: x[1])
-    print(list3)
-    for x in list3:
-        print (tweetIds[x[0]])
+            print("calculating similarity")
+            # print(cosine_similarity([query_embed],sen_embeddings[0:]))
+
+            for i in cosine_similarity([query_embed],sen_embeddings[0:]):
+                result = list(enumerate(i))
+                print(result)
+                list3 = sorted(result, reverse= True, key = lambda x: x[1])
+                print(list3)
+                
+                for x in list3:
+                    print (tweetIds[x[0]])
